a94ef2da8cd1bf3222e62ea466967d170f7c13e3
Jinqiang <cuijinqiang@gmail.com>
2014-01-09 11:27:08 -0800

    add for loop in c++

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -3,5 +3,10 @@ using namespace std;
 main()
 {
     cout << "Hello World!";
+	for(int i = 0; i < 10 ; ++i )
+	{
+		cerr<< " test cerr" << i << endl;
+		cout<< " test cout" << 2*i << endl;
+	}
     return 0;
 }
